a38ec971258c792777c9c7c16606732dbfd247df
DeevMohith001 <mohitrajdeev2006@gmail.com>
2026-09-21 05:14:00 +0530

    worked on path parameters

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Path
 import json
 
 app = FastAPI()
@@ -22,3 +22,22 @@ def about():
 def view():
     data = load_data()
     return data
+
+
+@app.get('/patient/{patient_id}')
+def view_patient(patient_id: str = Path(..., description='ID of the patient in the DB', example='p001')):
+    # load all the patients
+    data = load_data()
+
+    if patient_id in data:
+        return data[patient_id]
+    return {'error':'patient not found'}
+
+
+
+"""
+Notes:
+--> Path parameters are the dynamic segmentsof a URL path used to identify a specific resource
+--> The Path() function in FastAPI is used to provide metadata validation rules, and documentation hints for the path parameters in your API endpoints
+
+"""
