5739068984e9303fad3d785dde193785e9d1c3ed
DeevMohith001 <mohitrajdeev2006@gmail.com>
2026-09-21 05:51:27 +0530

    worked on query parameters

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, HTTPException, Path
+from fastapi import FastAPI, HTTPException, Path, Query
 import json
 
 app = FastAPI()
@@ -34,10 +34,32 @@ def view_patient(patient_id: str = Path(..., description='ID of the patient in t
     raise HTTPException(status_code=404, detail='Patient not found')
 
 
+@app.get('/sort')
+def sort_patients(sort_by: str = Query(..., description='Sort on the basis of height, weight or bmi'), order: str = Query('asc', description='Sort in asc or desc order')):
+    valid_fields = ['height',  'weight', 'bmi']
+
+    if sort_by not in valid_fields:
+        raise HTTPException(status_code=400, detail=f'Invalid field select from {valid_fields}')
+
+    if order not in ['asc', 'desc']:
+        raise HTTPException(status_code=400, detail='Invalid order select between asc and desc')
+
+    data = load_data()
+
+    sort_order = True if order=='desc' else False
+
+    sorted_data = sorted(data.values(), key=lambda x: x.get(sort_by, 0), reverse=sort_order)
+
+    return sorted_data
+
+
+
 
 """
 Notes:
 --> Path parameters are the dynamic segmentsof a URL path used to identify a specific resource
 --> The Path() function in FastAPI is used to provide metadata validation rules, and documentation hints for the path parameters in your API endpoints
+--> Query parameters are the optional key-value pairs appended to the end of a URL, used to pass additional data to the server in an HTTP request.
+    They are typically employed for operations like filtering, sorting, searching and  pagination without altering the endpoint path itself.
 
 """
