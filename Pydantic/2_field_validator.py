a4746b23f46911b386e5d227f5417a0d3d41ade8
DeevMohith001 <mohitrajdeev2006@gmail.com>
2026-09-23 01:23:11 +0530

    worked on model validator in pydantic

--- Pydantic/2_field_validator.py
+++ Pydantic/2_field_validator.py
@@ -51,3 +51,5 @@ patient_info = {'name':'nitish', 'email':'abc@icici.com', 'age': '30', 'weight':
 patient1 = Patient(**patient_info) # validation -> type coercion
 
 update_patient_data(patient1)
+
+# Field validator operates on a single fiels
